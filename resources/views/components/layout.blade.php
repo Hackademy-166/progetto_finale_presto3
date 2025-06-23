434c7acdd0134ae99861aca032147247e4140b24
Sabrina Fabbrini <sbrnfabbrini@gmail.com>
2025-06-23 12:14:01 +0200

    inseriti elementi components, aggiornato readme, inseirta palette e font su foglio css

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- --font-- --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=LXGW+Marker+Gothic&family=Savate:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+
+    {{-- vite --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <title>PRESTO.IT</title>
+</head>
+<body>
+    <x-navbar/>
+    
+    <div class="min-vh-100">
+
+        {{$slot}}
+
+    </div>
+
+    <x-footer/>
+</body>
+</html>
