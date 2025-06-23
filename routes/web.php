5a9f4f529e2c659401b3b64e39f53dce1faa5891
Emiliano Del Vasto <emiliano.delvasto@gmail.com>
2025-06-23 15:27:50 +0200

    stiamo facendo la welcome

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 
-Route::get('/', [PublicController::class, 'home'])->name('home');
+Route::get('/', [PublicController::class, 'home'])->name('homepage');
