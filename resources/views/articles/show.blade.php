f3e9fd88338516173100d46a14e790e7dfc49c42
Sabrina Fabbrini <sbrnfabbrini@gmail.com>
2025-06-24 13:06:06 +0200

    sistemata vista pagina show

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,19 +1,24 @@
 <x-layout>
-    <h1>DETTAGLIO ARTICOLO {{$article->title}}</h1>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <x-card-orizontal :article="$article" />
-            </div>
-        </div>
-    </div>
-    <section>
-        <div class="container">
-            <div class="row">
+
+    <h1 class="text-dark text-center font-title display-3 pt-3">DETTAGLIO ARTICOLO: {{$article->title}}</h1>
+
+        <div class="container bg-form shadow shadow-2 p-5">
+            <div class="row justify-content-center align-items-center ">
                 <div class="col-12 col-md-6">
-                    {{-- CAROSELLO --}}
+                    <h5 class=" font-extra">Titolo: {{$article->title}}</h5>
+            <p class="font-text">Descrizione: {{$article->description}}</p>
+            <p class=" font-extra">Prezzi: {{$article->price}}</p>
+            <a href="" class="link-offset-2 link-underline link-underline-opacity-0 text-color font-text">Categoria: {{$article->category->category_name}}</a>
                 </div>
             </div>
         </div>
-    </section>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        {{-- CAROSELLO --}}
+                    </div>
+                </div>
+            </div>
+        </section>
 </x-layout>
--- resources/views/components/card-orizontal.blade.php
+++ resources/views/components/card-orizontal.blade.php
@@ -5,10 +5,10 @@
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title font-title">Titolo{{$article->title}}</h5>
-        <p class="card-text font-text">Descrizione{{$article->description}}</p>
-        <p class="card-text font-title">Prezzi{{$article->price}}</p>
-        <a href="" class="">Categoria:{{$article->category->category_name}}</a>
+        <h5 class="card-title font-title">Titolo: {{$article->title}}</h5>
+        <p class="card-text font-text">Descrizione: {{$article->description}}</p>
+        <p class="card-text font-title">Prezzi: {{$article->price}}</p>
+        <a href="" class="">Categoria: {{$article->category->category_name}}</a>
         <a href="{{route('articles.show', $article)}}">Vai al dettaglio</a>
       </div>
     </div>
