a04dac84f8df57ad2623d30a71078f1641490780
Adriano Osorio <adriano_osr@hotmail.it>
2025-06-27 00:19:21 +0200

    Rivisitata pagina profilo dell'utente

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -7,29 +7,27 @@
             </div>
         </div>
     </header>
-    <section class="container">
-        <div class="row">
-            <div class="col-12 col-md-6 col-lg-6">
-                <div class="card text-center">
-                    <div class="card-header">
-                        <ul class="nav nav-tabs card-header-tabs">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="true">Dati Utente</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link">Dati Profilo</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled">Articoli dell'utente</a>
-                        </li>
-                        </ul>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                    </div>
+    <section>
+        <div class="container-dashboard">
+            <div class="card-header">
+                <ul class="nav nav-pills card-header-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="true" href="#">Dati utente</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Dati utente</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title mt-3">{{Auth::user()->name}}</h5>
+                <p class="card-text">Email: {{Auth::user()->email}}</p>
+                <p class="card-text"><small>Utente dal: {{Auth::user()->created_at->format('d/m/Y')}}</small></p>
+                <a class="text-dark btn bg-button linker " onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Log-Out</a>
+                <form action="{{route('logout')}}" method="POST" class="d-none" id="form-logout">@csrf</form>
             </div>
         </div>
     </section>
