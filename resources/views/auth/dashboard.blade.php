db7c7858813e334c6d534e46e83785ad4fd22193
Luca Delgiudice <lucadelgiudice9@gmail.com>
2025-06-26 17:06:46 +0200

    implementazione profilo utente

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-slot name="title">Dashboard</x-slot>
+    <x-slot name="title">Dashboard {{Auth::user()->name}}</x-slot>
     <header class="container vh-auto">
         <div class="row">
             <div class="col-12 col-md-12 col-lg-12">
@@ -7,4 +7,30 @@
             </div>
         </div>
     </header>
+    <section class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-6">
+                <div class="card text-center">
+                    <div class="card-header">
+                        <ul class="nav nav-tabs card-header-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="true">Dati Utente</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Dati Profilo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled">Articoli dell'utente</a>
+                        </li>
+                        </ul>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+            </div>
+        </div>
+    </section>
 </x-layout>
