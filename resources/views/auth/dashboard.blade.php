cd1e37b7ec8c91920ca40219fcd10208ece7754d
Adriano Osorio <adriano_osr@hotmail.it>
2025-06-30 09:59:39 +0200

    Apportate alcune modifiche minori

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -24,7 +24,7 @@
                                 @endif
                                 <br>
                                 <button type="button" class="btn bg-button mt-5 ms-1" data-bs-toggle="modal" data-bs-target="#exampleModal">Elimina</button>
-                                <button class=" mt-5 ms-1 btn bg-button">Modifica</button>
+                                <button class="d-none mt-5 ms-1 btn bg-button">Modifica</button>
                             </div>
                         </div>
                     </div>
@@ -59,10 +59,10 @@
         <div class="row">
             <div class="col-12 col-md-4 col-lg-4 "></div>
             <div class="col-12 col-md-4 col-lg-4">
-                <div class="card">
+                <div class="card mt-3">
                     <div class="card-body">
                         <div class="d-flex flex-column align-items-center text-center">
-                            <div class="mt-3">
+                            <div class="">
                                 <h4>Articoli Pubblicati: <strong id="article-count" data-final="{{ $user->articles_count }}">0</strong></h4>
                                 <h4>Annunci Venduti: <span id="soldArticles">0</span></h4>
                             </div>
