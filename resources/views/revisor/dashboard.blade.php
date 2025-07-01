a15e3ca774a17c10490440a1e484570b1525d240
Lorenzo Gatti <lorenzo2gatti9@gmail.com>
2025-07-01 17:57:09 +0200

    user story 5 e 6 fubnzionante

--- resources/views/revisor/dashboard.blade.php
+++ resources/views/revisor/dashboard.blade.php
@@ -9,30 +9,55 @@
     </header>
     
     @if(session()->has('message'))
-        <div class="alert alert-succes">
-            {{session('message')}}
-        </div>
+    <div class="alert alert-succes">
+        {{session('message')}}
+    </div>
     @endif
-
+    
     <section class="container-fluid">
         <div class="row">
-            @if($article_to_check && $article_to_check->images->count())
-                @foreach (Sarticle_to_check->images as $key=> $image)
-                    <div class="col-6 col-md-4 mb-4">
-                        <img src="{{$image->getUrl(300, 300)}}" class="img-fluid rounded shadow" alt="Immagine {($key +1 }} dell'articolo '{{$article_to_check->title}}">
-                    </div>
-                @endforeach
-            @else
-                @for ($i = 0; $i < 6; $i++)
-                    <div class="col-12 col-md-3 col-lg-3 mb-4 text-center">
-                        <img src="https://picsum.photos/300" alt="immagine segnaposto" class="img-fluid rounded shadow">
-                    </div>
-                @endfor
+            @if($article_to_check)
+                @if( $article_to_check->images->count())
+                
+                    @foreach ($article_to_check->images as $key=> $image)
+                    
+                        <div class="col-6 col-md-4 mb-4">
+                            <img src="{{$image->getUrl(300, 300)}}" class="img-fluid rounded shadow" alt="Immagine {{$key +1 }} dell'articolo '{{$article_to_check->title}}">
+                        </div>
+                    @endforeach
+                @else
+                    @for ($i = 0; $i < 6; $i++)
+                        <div class="col-12 col-md-3 col-lg-3 mb-4 text-center">
+                            <img src="https://picsum.photos/300" alt="immagine segnaposto" class="img-fluid rounded shadow">
+                        </div>
+                    @endfor
+                @endif
+                <div class="col-md-4 ps-4 d-flex flex-column justify-content-between">
+                <div>
+                    <h1>{{ $article_to_check->title }}</h1>
+                    <h3>Autore: {{ $article_to_check->user->name }} </h3> <h4>{{ $article_to_check->price }}€</h4>
+                    <h4 class="fst-italic text-muted">{{ $article_to_check->category->category_name }}</h4>
+                    <p class="h6">{{ $article_to_check->description }}</p>
+                </div>
+                <div class="d-flex pb-4 justify-content-around">
+                    <form action="{{route('reject', ['article'=> $article_to_check])}}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <button class="btn btn-danger py-2 px-5 fw-bold">Rifiuta</button>
+                    </form>
+                    <form action="{{route('accept', ['article'=> $article_to_check])}}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <button class="btn btn-success py-2 px-5 fw-bold">Accetta</button>
+                    </form>
+                </div>
+            </div>
             @endif
+            
         </div>
     </section>
-
-    <section class="container">
+    
+    {{-- <section class="container">
         @if ($article_to_check)
         <div class="row justify-content-center pt-5">
             <div class="col-md-8">
@@ -74,5 +99,5 @@
         </div>
         @endif
         <a href="{{ route('homepage') }}" class="mt-5 btn btn-success"> Torna </a>
-        </section>
-    </x-layout>
+    </section> --}}
+</x-layout>
