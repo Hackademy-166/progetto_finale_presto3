2586996c3461909428565b82b108695ddf8a09a3
Adriano Osorio <adriano_osr@hotmail.it>
2025-07-01 13:04:59 +0200

    commit prima del pull

--- resources/views/livewire/create-article.blade.php
+++ resources/views/livewire/create-article.blade.php
@@ -4,22 +4,22 @@
             <div class="col-12 col-md-6">
                 
                 {{-- @if(session('message'))
-                    <div id="timer_alert" class="alert alert-success" wire:ignore>
-                        {{ session('message') }}
-                    </div>
+                <div id="timer_alert" class="alert alert-success" wire:ignore>
+                    {{ session('message') }}
+                </div>
                 @endif
                 
                 @push('scripts')
-                    <script>
-                        document.addEventListener('DOMContentLoaded', function () {
-                        const alertBox = document.querySelector('#timer_alert');
-                        if (alertBox) {
-                        setTimeout(function () {
-                        alertBox.classList.add('d-none');
-                        }, 3000); // 3 secondi
-                        }
-                        });
-                    </script>
+                <script>
+                    document.addEventListener('DOMContentLoaded', function () {
+                    const alertBox = document.querySelector('#timer_alert');
+                    if (alertBox) {
+                    setTimeout(function () {
+                    alertBox.classList.add('d-none');
+                    }, 3000); // 3 secondi
+                    }
+                    });
+                </script>
                 @endpush
                 @stack('scripts') --}}
                 
@@ -135,7 +135,7 @@
                     <script>
                         let btn_trigger=document.querySelector('#btn_trigger');
                         let timer_alert=document.querySelector('#timer_alert');
-
+                        
                         btn_trigger.addEventListener('click', function() {
                             timer_alert.classList.add('sparisci');
                         });                        
@@ -166,34 +166,33 @@
                         <span class="fst-italic text-danger">{{$message}}</span>
                         @enderror
                     </div>
-
+                    
                     <div class="mb-3">
                         <input type="file" wire:model.live="temporary_images" multiple class="form-control shadow @error ('temporary_images.*') is-invalid @enderror" placeholder="Img/">
                         @error ('temporary_images.*')
-                            <p class="fst-italic text-danger">{{ $message }}</p>
+                        <p class="fst-italic text-danger">{{ $message }}</p>
                         @enderror
                         @error ('temporary_images')
-                            <p class="fst-italic text-danger">({ $message })</p>
+                        <p class="fst-italic text-danger">({ $message })</p>
                         @enderror
                     </div>
                     @if (!empty($images))
                     
-                        <p>Photo preview:</p>
-                        <div class="row border border-4 border-success rounded shadow py-4">
-                    @foreach ($images as $key => $image)
-                            <div class="col d-flex flex-column align-items-center my-3">
-                                <div class="img-preview mx-auto shadow rounded" style="background-image: url({{ $image->temporaryUrl() }});"></div>
-                                <button type="button" wire:click="removeImage({{ $key }})" class="btn btn-danger mt-2">Rimuovi</button>
-                            </div>
-                    @endforeach
-                    @endif
-                                </div>
+                    <p>Photo preview:</p>
+                    <div class="row border border-4 border-success rounded shadow py-4">
+                        @foreach ($images as $key => $image)
+                        <div class="col d-flex flex-column align-items-center my-3">
+                            <div class="img-preview mx-auto shadow rounded" style="background-image: url({{ $image->temporaryUrl() }});"></div>
+                            <button type="button" wire:click="removeImage({{ $key }})" class="btn btn-danger mt-2">Rimuovi</button>
+                        </div>
+                        @endforeach
+                    </div>
                     @endif     
                     <div class="mb-3">
                         <label for="category" class="form-label">Categoria</label>
                         <select class="form-select" id="category" required wire:model="category">
                             @foreach ($categories as $category)
-                                <option value="{{$category->id}}">{{$category->category_name}}</option>
+                            <option value="{{$category->id}}">{{$category->category_name}}</option>
                             @endforeach
                         </select>
                     </div>
