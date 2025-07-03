b8c052925d49850c8cd4c432c84b7af186c8a889
Luca Delgiudice <lucadelgiudice9@gmail.com>
2025-07-03 15:00:08 +0200

    aggiunta immagine remove-faces

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/make/revisor/{user}', [RevisorController::class, 'makeRevisor'])->n
 Route::get('/revisor/dashboard', [RevisorController::class, 'index'])->name('revisor.dashboard')->middleware('isRevisor'); // ??rotta paremtrica o no?
 Route::patch('/accept/{article}', [RevisorController::class, 'accept'])->name('accept')->middleware('isRevisor');
 Route::patch('/reject/{article}', [RevisorController::class, 'reject'])->name('reject')->middleware('isRevisor');
-Route::patch('/change/{article}', [RevisorController::class, 'changeStatus'])->middleware('isRevisor')->name('change');
+// Route::patch('/change/{article}', [RevisorController::class, 'changeStatus'])->middleware('isRevisor')->name('change');
 
 
 // ROTTE LINGUE
